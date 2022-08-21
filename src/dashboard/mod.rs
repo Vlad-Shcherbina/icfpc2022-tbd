fdc6fa70e1442a30e4207bc060d0e673b62daf28
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-08-21 17:03:56 +0200

    HTML templates

--- src/dashboard/mod.rs
+++ src/dashboard/mod.rs
@@ -1,6 +1,7 @@
 mod dev_server;
 mod static_files;
 
+use askama::Template;
 use crate::util::project_root;
 
 crate::entry_point!("dashboard", dashboard);
@@ -9,9 +10,22 @@ fn dashboard() {
     eprintln!("serving at http://127.0.0.1:8000 ...");
     dev_server::serve_forever(listener, |req, resp| {
         if req.path == "/" {
-            return resp.code("200 OK").body("hello");
+            let s = HelloTemplate { username: "username" }.render().unwrap();
+            return resp.code("200 OK").body(s);
         }
 
         static_files::static_handler(&project_root(), req, resp)
     });
+}
+
+#[derive(Template)]
+#[template(ext = "html", source = r#"
+{% extends "base.html" %}
+{% block title %}hello{% endblock %}
+{% block body %}
+Hello, <b>{{ username }}</b>!
+{% endblock %}
+"#)]
+struct HelloTemplate<'a> {
+    username: &'a str,
 }
