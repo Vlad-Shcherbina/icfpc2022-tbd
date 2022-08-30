3f607b6d900f888e3469ed7db1078dc41d37cf46
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-08-30 17:09:33 +0200

    Table of contents for dashboard examples

--- src/dashboard/examples.rs
+++ src/dashboard/examples.rs
@@ -2,11 +2,20 @@ use askama::Template;
 use super::dev_server::{Request, ResponseBuilder, HandlerResult};
 
 pub fn handler(req: Request, resp: ResponseBuilder) -> HandlerResult {
+    if req.path.is_empty() {
+        let s = ExamplesTemplate {}.render().unwrap();
+        return resp.code("200 OK").body(s);
+    }
     if let Some(name) = req.path.strip_prefix("hello/") {
         let s = HelloTemplate { name }.render().unwrap();
         return resp.code("200 OK").body(s);
     }
     resp.code("400 Not Found").body("not found")
+}
+
+#[derive(Template)]
+#[template(path = "examples.html")]
+struct ExamplesTemplate {
 }
 
 #[derive(Template)]
