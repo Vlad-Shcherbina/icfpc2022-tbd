a711a33ec936ac4fe3ea1eb072fbe241e67fe892
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-09-02 23:27:37 +0200

    "solutions" table schema

--- src/db.rs
+++ src/db.rs
@@ -8,8 +8,18 @@ crate::entry_point!("init_tables", init_tables);
 fn init_tables() {
     let mut client = create_client();
     client.batch_execute(r#"
-    CREATE TABLE IF NOT EXISTS zzz (
-        id SERIAL PRIMARY KEY
+    CREATE TABLE IF NOT EXISTS solutions(
+        id SERIAL PRIMARY KEY,
+        problem_id INTEGER NOT NULL,  -- could be foreign key, but maybe we don't want to bother with the "problems" table
+
+        data TEXT NOT NULL,  -- move instructions as text
+        moves_cost INTEGER NOT NULL,
+        image_distance INTEGER NOT NULL,
+
+        solver TEXT NOT NULL,  -- name of the solver
+        solver_args JSONB NOT NULL,  -- any solver-specific parameters, just to understand what's going on
+        invocation_id INTEGER NOT NULL REFERENCES invocations_raw(id),
+        timestamp TIMESTAMP WITH TIME ZONE NOT NULL
     );
     "#).unwrap();
 
