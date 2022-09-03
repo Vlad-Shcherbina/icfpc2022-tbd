1f51abf20af2a5c0eccd674d33172872e1668b0a
cognivore <noreply>
2022-09-03 14:07:19 +0100

    Full spec first batch

--- data/dumb_scrape.sh
+++ data/dumb_scrape.sh
@@ -2,8 +2,12 @@
 
 onwards="true"
 
-for x in {1..100}; do
+for x in {1..1000}; do
     read -rt0.2
     wget "https://cdn.robovinci.xyz/imageframes/$x.png" -O "data/problems/$x.png" || onwards="";
     [ -z "$onwards" ] && rm "data/problems/$x.png" && break
+    if [[ $x -gt 25 ]] ; then
+        wget "https://cdn.robovinci.xyz/imageframes/$x.initial.json" -O "data/problems/$x.initial.json"
+        wget "https://cdn.robovinci.xyz/imageframes/$x.initial.png" -O "data/problems/$x.initial.png"
+    fi
 done
