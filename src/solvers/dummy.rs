f3052c846ce351b5f1bcb7e19989410b07b4618e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2022-09-04 19:42:29 +0200

    Update dummy solver template (support "--problem 1..20" and --dry-run)

--- src/solvers/dummy.rs
+++ src/solvers/dummy.rs
@@ -8,50 +8,59 @@
 // (preferably ones that are easy to pronounce too).
 // Don't copy this comment when you copy the code.
 
+#![allow(unused_imports)]
+
+use crate::util::project_path;
 use crate::basic::*;
+use crate::image::Image;
 use crate::invocation::{record_this_invocation, Status};
 use crate::uploader::upload_solution;
+use crate::color_util::*;
+use crate::seg_util;
+
+use crate::basic::Move::*;
+
+#[derive(serde::Serialize)]
+#[derive(Debug)]
+struct SolverArgs {
+    px: i32,
+    py: i32,
+    num_colors: usize,
+}
 
 crate::entry_point!("dummy_solver", dummy_solver);
 fn dummy_solver() {
-    let args: Vec<String> = std::env::args().skip(2).collect();
-    if args.len() != 1 {
-        println!("Usage: dummy_solver <problem id>");
-        std::process::exit(1);
-    }
-    let problem_id: i32 = args[0].parse().unwrap();
-    let problem = Problem::load(problem_id);
+    let mut pargs = pico_args::Arguments::from_vec(std::env::args_os().skip(2).collect());
+    let problems: String = pargs.value_from_str("--problem").unwrap();
+    let dry_run = pargs.contains("--dry-run");
+    let rest = pargs.finish();
+    assert!(rest.is_empty(), "unrecognized arguments {:?}", rest);
 
+    let problem_range = crate::util::parse_range(&problems);
+    let mut client = crate::db::create_client();
+    for problem_id in problem_range {
+        eprintln!("*********** problem {} ***********", problem_id);
+        let problem = Problem::load(problem_id);
+        let moves = solve(&problem);
+        let mut tx = client.transaction().unwrap();
+        let incovation_id = record_this_invocation(&mut tx, Status::Stopped);
+        upload_solution(&mut tx, problem_id, &moves, "dummy", &serde_json::Value::Null, incovation_id);
+        if dry_run {
+            eprintln!("But not really, because it was a --dry-run!");
+            break;  // because record_this_invocation() doesn't play well with reverted transactions
+        } else {
+            tx.commit().unwrap();
+        }
+    }
+}
+
+fn solve(problem: &Problem) -> Vec<Move> {
     let color = crate::color_util::optimal_color_for_block(
         &problem.target, &Shape { x1: 0, y1: 0, x2: problem.target.width, y2: problem.target.height });
-
-    let moves = vec![
+    vec![
         Move::ColorMove {
             block_id: BlockId::root(0),
             color,
-        }
-    ];
-
-    let mut painter = PainterState::new(&problem);
-    for m in &moves {
-        eprintln!("{}", m);
-        painter.apply_move(m);
-    }
-
-    let img = painter.render();
-    eprintln!();
-    eprintln!("cost: {}", painter.cost);
-    let dist = image_distance(&img, &problem.target);
-    eprintln!("distance to target: {}", dist);
-    eprintln!("final score: {}", dist.round() as i64 + painter.cost);
-
-    let output_path = format!("outputs/dummy_{}.png", problem_id);
-    img.save(&crate::util::project_path(&output_path));
-    eprintln!("saved to {}", output_path);
-
-    let mut client = crate::db::create_client();
-    let mut tx = client.transaction().unwrap();
-    let incovation_id = record_this_invocation(&mut tx, Status::Stopped);
-    upload_solution(&mut tx, problem_id, &moves, "dummy", &serde_json::Value::Null, incovation_id);
-    tx.commit().unwrap();
+        },
+    ]
 }
